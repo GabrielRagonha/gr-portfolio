f67b9a50662f8a785d97b247586d366625161d61
Gabriel Rodrigues <gabrielragonhaprofissional@outlook.com>
2025-01-19 17:54:53 -0300

    build: initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <div className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">Gabriel</h1>
+        </Link>
+
+        {/* Desktop nav */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Me contrate</Button>
+          </Link>
+        </div>
+
+        {/* Mobile nav */}
+        <div className="xl:hidden"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
